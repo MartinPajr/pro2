1311bbbd698caba719c5f5392590cc131a8cf1d7
pajrma1 <pajrmartin@gmail.com>
2021-09-20 12:30:22 +0200

    menuItem added

---
+++ src/com/company/menuItem.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class menuItem {
+    public String name;
+    public String description;
+
+    private int price;
+
+    public menuItem(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
